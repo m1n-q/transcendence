7f8de2a94eb160f0d718c8f20af30e21d210e6ed
m1n-q <m1nq.dev@gmail.com>
2022-10-30 00:58:49 +0900

    chore: remove views

--- ws/src/main.ts
+++ ws/src/main.ts
@@ -1,14 +1,10 @@
 import { NestFactory } from '@nestjs/core';
-import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create(AppModule);
   const PORT = parseInt(process.env.SERVER_PORT);
 
-  app.useStaticAssets(__dirname + '/../static');
-  app.setBaseViewsDir(__dirname + '/../views');
-  app.setViewEngine('hbs'); //* set template engine
   await app.listen(PORT, () => console.log(`ws-server on ${PORT}`));
 }
 bootstrap();
