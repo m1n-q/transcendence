2e47e3b0b04abf00276ebf9f14c0e04944ad4cc3
m1n-q <m1nq.dev@gmail.com>
2022-10-29 23:17:11 +0900

    chore: restructure directory

--- notification-service/src/rmq/types/rmq-event.ts
+++ notification-service/src/rmq/types/rmq-event.ts
@@ -2,7 +2,7 @@ import { IsDateString, IsNotEmpty, IsString } from 'class-validator';
 
 /*
  @ routing key will be event type
- @ ex) user.event.friend-request.rk
+ @ ex) event.on.<service name>.<event type>[.additional.param].rk
  */
 export class RmqEvent {
   @IsString({ each: true })
@@ -13,4 +13,10 @@ export class RmqEvent {
 
   @IsDateString()
   created: Date;
+
+  constructor(payload, recvUsers: string[] = null) {
+    this.payload = payload;
+    this.recvUsers = recvUsers;
+    this.created = new Date();
+  }
 }
