227092cd69e2e10b74f813ebacb74a32e25a095c
toh <toh@tohui-MacBookPro.local>
2022-11-18 14:41:06 +0900

    Refactor: change dir name and websocket message

---
+++ game-ws/src/app.module.ts
@@ -0,0 +1,32 @@
+import { RabbitMQModule } from '@golevelup/nestjs-rabbitmq';
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AuthService } from './auth/auth.service';
+import { GameGateway } from './game/game.gateway';
+import { UserService } from './user/user.service';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env'],
+    }),
+    RabbitMQModule.forRoot(RabbitMQModule, {
+      uri: process.env.RMQ_HOST,
+      enableControllerDiscovery: true,
+      exchanges: [
+        {
+          name: 'auth.d.x',
+          type: 'direct',
+        },
+        {
+          name: 'user.d.x',
+          type: 'direct',
+        },
+      ],
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AuthService, UserService, GameGateway],
+})
+export class AppModule {}
