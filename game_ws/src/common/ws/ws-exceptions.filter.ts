d87c4a02682d09dc33c81f742fd9982dcfd5b016
toh <toh@tohui-MacBookPro.local>
2022-11-18 14:52:57 +0900

    Remove: game_ws

--- game_ws/src/common/ws/ws-exceptions.filter.ts
+++
@@ -1,9 +0,0 @@
-import { Catch } from '@nestjs/common';
-import { BaseWsExceptionFilter, WsException } from '@nestjs/websockets';
-
-@Catch(WsException)
-export class WsExceptionsFilter extends BaseWsExceptionFilter {
-  catch(exception, host) {
-    super.catch(exception, host);
-  }
-}
