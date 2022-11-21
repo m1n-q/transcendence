20868495ab00c6169f9da371e4fd9c03e792b300
m1n-q <m1nq.dev@gmail.com>
2022-11-21 19:08:45 +0900

    feat: add refresh-jwt API

--- gateway/src/auth/auth.controller.ts
+++ gateway/src/auth/auth.controller.ts
@@ -11,4 +11,9 @@ export class AuthController {
   async oauth(@Body('code') code, @Param('provider') provider: Provider) {
     return this.authService.requestSignIn(provider, code); // 가입되어 있다면 {access_token, refresth_token} (jwt), 아닐 시 RmqError(404)가 리턴됩니다
   }
+
+  @Post('/refresh')
+  async refresh(@Body('refresh_token') token) {
+    return this.authService.requestRefresh(token);
+  }
 }
--- gateway/src/auth/auth.service.ts
+++ gateway/src/auth/auth.service.ts
@@ -29,4 +29,24 @@ export class AuthService {
       );
     return response.data;
   }
+
+  async requestRefresh(token) {
+    let response: RmqResponse<Tokens>;
+    type Tokens = { access_token: string; refresh_token: string };
+    try {
+      response = await this.amqpConnection.request<RmqResponse<Tokens>>({
+        exchange: 'auth.d.x',
+        routingKey: 'req.to.auth.refresh.jwt.rk',
+        payload: { refresh_token: token },
+      });
+    } catch (reqFail) {
+      throw new InternalServerErrorException('request to auth-service failed');
+    }
+    if (!response.success)
+      throw new HttpException(
+        `${response.error.message} / where: ${response.error.where}`,
+        response.error.code,
+      );
+    return response.data;
+  }
 }
