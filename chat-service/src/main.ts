96baaa14a37c562298234e36ccc9d6a0c6afe9cf
m1n-q <m1nq.dev@gmail.com>
2022-11-25 17:46:00 +0900

    test: remove all connections to print envs

--- chat-service/src/main.ts
+++ chat-service/src/main.ts
@@ -4,6 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = parseInt(process.env.SERVER_PORT);
+  console.log(process.env.PG_HOST);
+  console.log(parseInt(process.env.PG_PORT));
+  console.log(process.env.PG_USERNAME);
+  console.log(process.env.PG_PASSWORD);
+  console.log(process.env.PG_DATABASE);
   await app.listen(port, () => console.log(`chat server on ${port}`));
 }
 bootstrap();
