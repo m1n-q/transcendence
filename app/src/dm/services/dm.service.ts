41d6c90aa12c648336f0426d21d601bd91f657ab
m1n-q <m1nq.dev@gmail.com>
2022-11-28 18:38:55 +0900

    feat: implement fetching stored DM API

--- app/src/dm/services/dm.service.ts
+++ app/src/dm/services/dm.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { plainToInstance } from 'class-transformer';
 import { Repository } from 'typeorm';
 import { DM } from '../../common/entities/dm.entity';
 import { toRmqError } from '../../common/rmq/errors/to-rmq-error';
+import { UserProfile } from '../../user-info';
 import { DmGetMessagesDto } from '../dto/dm-get-messages.dto';
 import { DmDto } from '../dto/dm.dto';
 
@@ -28,13 +30,36 @@ export class DmService {
   async getAllMessages(data: DmGetMessagesDto) {
     const messages = await this.dmRepository
       .find({
-        where: { receiverId: data.receiver_id, senderId: data.sender_id },
-        order: { created: 'DESC' },
+        where: [
+          { receiverId: data.receiver_id, senderId: data.sender_id },
+          { senderId: data.receiver_id, receiverId: data.sender_id },
+        ],
+
+        order: { created: 'ASC' },
+        relations: ['sender', 'receiver'],
       })
       .catch((e) => {
         throw toRmqError(e);
       });
-
-    return messages;
+    return {
+      messages: messages.map((dm) => {
+        const sender: UserProfile = plainToInstance(UserProfile, dm.sender, {
+          excludeExtraneousValues: true,
+        });
+        const receiver: UserProfile = plainToInstance(
+          UserProfile,
+          dm.receiver,
+          {
+            excludeExtraneousValues: true,
+          },
+        );
+        return {
+          sender,
+          receiver,
+          payload: dm.payload,
+          created: dm.created,
+        };
+      }),
+    };
   }
 }
