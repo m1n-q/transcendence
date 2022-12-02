fcbf6cfbf971c853a621da3da48fe921d8b4036d
m1n-q <m1nq.dev@gmail.com>
2022-12-02 16:29:38 +0900

    refactor: modify RmqEvent properties

--- app/src/common/rmq/types/rmq-event.ts
+++ app/src/common/rmq/types/rmq-event.ts
@@ -4,18 +4,18 @@ import { IsDateString, IsNotEmpty, IsString } from 'class-validator';
  @ routing key will be event type
  @ ex) event.on.<service name>.<event type>[.additional.param].rk
  */
-export class RmqEvent {
+export class RmqEvent<T = any> {
   @IsString({ each: true })
   recvUsers: string[];
 
   @IsNotEmpty()
-  payload: any;
+  data: T;
 
   @IsDateString()
   created: Date;
 
-  constructor(payload, recvUsers: string[] = null) {
-    this.payload = payload;
+  constructor(data: T, recvUsers: string[] = null) {
+    this.data = data;
     this.recvUsers = recvUsers;
     this.created = new Date();
   }
