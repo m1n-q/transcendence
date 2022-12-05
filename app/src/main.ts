624231b7d84a9a9897a5dc84795e60c0545d1925
m1n-q <m1nq.dev@gmail.com>
2022-12-05 13:57:53 +0900

    test: add unhandledRejection handler to debug

--- app/src/main.ts
+++ app/src/main.ts
@@ -2,6 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  process.on('unhandledRejection', (error) => {
+    console.error(error); // This prints error with stack included (as for normal errors)
+    throw error; // Following best practices re-throw error and let the process exit with error code
+  });
+
   const app = await NestFactory.create(AppModule);
   const PORT = parseInt(process.env.SERVER_PORT);
 
